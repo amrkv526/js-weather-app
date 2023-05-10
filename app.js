bf5bb45f5855690312d2b46daaa27ac5d80f3eb9
amrkv526 <amarkev@gmail.com>
2023-05-10 10:01:15 -0400

    updated API key placeholder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
-const apiKey = "";
+const apiKey = "YOUR API KEY HERE";
 
 let fetchedCities = [];
 
